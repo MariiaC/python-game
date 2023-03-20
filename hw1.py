a04d5d4f979369a635d2866f9e7342c6f7cbb804
MariiaC <mariiachvorun@gmail.com>
2023-03-20 21:42:43 +0200

    py-project

---
+++ hw1.py
@@ -0,0 +1,12 @@
+
+    # # ball_rect = ball_rect.move(ball_speed)
+
+    # pressed_keys = pygame.key.get_pressed()
+
+    # # if ball_rect.bottom >= heigth or ball_rect.top <= 0:
+    # #    ball.fill(BLUE)   
+    # #    ball_speed[1] = -ball_speed[1] 
+
+    # # if ball_rect.right >= width or ball_rect.left <= 0:
+    # #    ball.fill(YELLOW)
+    # #    ball_speed[0] = -ball_speed[0] 
